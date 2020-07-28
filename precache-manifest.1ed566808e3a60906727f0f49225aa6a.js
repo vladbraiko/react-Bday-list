e23bd4d1d51f101d46ee04c1c9df3f0cc2a5cbb4
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-07-28 22:52:28 +0300

    Updates

---
+++ precache-manifest.1ed566808e3a60906727f0f49225aa6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b683f32133e04069de15e48ccc9146a",
+    "url": "/react-Bday-list/index.html"
+  },
+  {
+    "revision": "113a8eabcec877ad461f",
+    "url": "/react-Bday-list/static/css/main.9e5e59c3.chunk.css"
+  },
+  {
+    "revision": "44946755c2a25ef8e829",
+    "url": "/react-Bday-list/static/js/2.21b77cc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Bday-list/static/js/2.21b77cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113a8eabcec877ad461f",
+    "url": "/react-Bday-list/static/js/main.0cbc4d15.chunk.js"
+  },
+  {
+    "revision": "0941b37464063e3ded86",
+    "url": "/react-Bday-list/static/js/runtime-main.584858b9.js"
+  }
+]);
